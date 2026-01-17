91b3d17daf799052e47dc90b217b5c92e64de04d
Paulius Bendaravičius <pauliusbendaravicius12@gmail.com>
2026-01-17 12:09:44 +0200

    chore: migrate project to ES modules

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
-import express, { Request, Response } from "express";
+import express from "express";
+
 const app = express();
-const port = 3000;
 
-app.get("/", (req: Request, res: Response): void => {
-  res.send("Hello World!");
-});
+app.listen(3000, () => console.log("Server running on port 3000"));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
