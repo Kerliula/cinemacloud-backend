01dab6ac7590dece61b79e60d3e397c2d3b9029d
Paulius Bendaravičius <pauliusbendaravicius12@gmail.com>
2026-01-17 19:40:36 +0200

    feat: enable CORS and add basic security headers with Helmet

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import dotenv from 'dotenv';
-import express from 'express';
+import cors from 'cors';
+import express, { type Application } from 'express';
+import helmet from 'helmet';
 
-dotenv.config();
+const app: Application = express();
 
-const app = express();
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT);
 
