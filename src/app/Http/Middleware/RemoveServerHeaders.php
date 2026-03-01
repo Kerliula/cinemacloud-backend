858366dc15237163f3f45d352b74e5625ee38310
Paulius Bendaravičius <pauliusbendaravicius12@gmail.com>
2026-03-01 20:37:32 +0200

    security: hide technology stack identifiers

---
+++ src/app/Http/Middleware/RemoveServerHeaders.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RemoveServerHeaders
+{
+    /**
+     * Handle an incoming request and remove identifying headers.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        // Remove headers that might identify the server technology
+        $response->headers->remove('X-Powered-By');
+        $response->headers->remove('Server');
+        
+        return $response;
+    }
+}
--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
+        // Remove identifying headers
+        $middleware->append(\App\Http\Middleware\RemoveServerHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
